e6f21a65971c15d2325398bb76670c17735a79d1
Giovanna Moraes <g.vaz.guimaraes@gmail.com>
2016-04-11 17:25:37 -0400

    initial commit, finalized homework javascript

---
+++ javacheatsheet.js
@@ -0,0 +1,94 @@
+// 1a adding two variables
+
+var x = 2;
+var y = 3;
+
+x+y;
+
+// 1b adding two string variables 
+
+var x = "Hello!";
+var y = " How are you?"
+
+x+y;
+
+//2 multidimentional array 
+var sports = [["swimming", "surfing"],["running", "cycling"]]
+
+console.log (sports[0][1])
+
+//3 checking if variable is less than 10 
+var lessThan= function(x,y){
+	if (x<y){
+		return alert([ x +" is less than "+ y]);
+	} else{
+		console.log ([x + " is greater than "+ y]);
+	};
+};
+
+lessThan(5,10); // will give you an alert saying that 5 is less than 10
+lessThan(11,10); // will print to console that 11 is greater than 10
+
+// 5 check if string equals another string 
+
+var stringCheck= function(x,y){ // specific to stirng variables
+	if (typeof x === "string" && typeof y === "string"){
+
+		if (x===y){
+			console.log("These strings match");
+		} else{
+			console.log("These strings do not match");
+		};
+	} else { 
+		console.log ("One or more of your inputs is not a stirng")
+	};
+};
+
+//6 return message or name string 
+function returnString (myMessage){
+// without an alert -- function takes a string and returns string
+	return (myMessage);
+}
+
+//7 function with no arguments but prints something to console 
+function noArguments (){
+	console.log ("Type something");
+}
+
+//Additional Problem with if loop 
+
+function pickDoor (selectedDoor){
+	if (selectedDoor === 1){
+		return "You get nothing"
+	} else if (selectedDoor === 2){
+		return "You get $1"
+	} else if (selectedDoor === 3){
+		return "You get a house!!!"
+	} else {
+		return "Door " + selectedDoor + " is not an option"
+	};
+};
+
+// using a for loop 
+var friendList = ["Kim", "Kassim", "Chris", "Mandisa", "Rick", "Sam"]; 
+
+for(var i =0; i<friendList.length; i++){
+   console.log (friendList[i]+ " is my friend");
+}
+
+//using a while loop 
+while (x<10){
+	console.log('on number ' + x);
+	x++;
+}
+
+ //using a while and if loop to perfect the beer bottle song 
+var beerNum=99;
+while (beerNum>0){
+	if (beerNum>1){
+		console.log (beerNum + " bottles of Beer on the wall, " + beerNum + " bottles of beer! Take one down, pass it around. " + (beerNum -=1)+ " bottles of beer on the wall!");
+	} else {
+		console.log (beerNum + " bottle of Beer on the wall, " + beerNum + " bottle of beer! Take it down, don't pass it around, " + (beerNum -=1)+ " bottles of beer on the wall!");
+	};
+};
+
